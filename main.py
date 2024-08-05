e5b89a510c23a2b4b26fee2b8aac4a5770a38bcd
WeatherboySuper <SuperPlays1020@gmail.com>
2024-08-05 09:19:44 -0500

    GUI Update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tkinter
 import customtkinter as ctk
 import threading
 import multiprocessing
+import alerts_main
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
@@ -17,27 +18,23 @@ root.title("OBS Weather Tools")
 main_frame = ctk.CTkFrame(master=root)
 main_frame.grid(row=0, column=0, padx=10, pady=10)
 
-alert_stop_event = threading.Event()
+live_alert_stop_event = threading.Event()
 dashboard_stop_event = threading.Event()
+alert_stop_event = threading.Event()
 
 alerts_thread = None
 dashboard_thread = None
 
-alert_var = tkinter.BooleanVar()
+live_alert_var = tkinter.BooleanVar()
 dashboard_var = tkinter.BooleanVar()
+alert_var = tkinter.BooleanVar()
 
 def update_dashboard_state(*args):
-    if alert_var.get():
+    if live_alert_var.get():
         dashboard_checkbox.configure(state="normal")
     else:
         dashboard_checkbox.configure(state="disabled")
         dashboard_var.set(False)
-
-alert_checkbox = ctk.CTkCheckBox(main_frame, text="Alert Monitor", variable=alert_var, command=update_dashboard_state)
-alert_checkbox.grid(row=0, column=0, padx=10, pady=10)
-
-dashboard_checkbox = ctk.CTkCheckBox(main_frame, text="Dashboard", variable=dashboard_var, state="disabled")
-dashboard_checkbox.grid(row=1, column=0, padx=10, pady=10)
 
 def start_dashboard():
     global dashboard_thread
@@ -53,7 +50,7 @@ def stop_dashboard():
 def confirm_action():
     global alerts_thread
 
-    if alert_var.get():
+    if live_alert_var.get():
         if not alerts_thread or not alerts_thread.is_alive():
             alert_stop_event.clear()
             alerts_thread = threading.Thread(target=kickstart, args=(alert_stop_event,))
@@ -68,9 +65,18 @@ def confirm_action():
     else:
         dashboard_stop_event.set()
 
+live_alert_checkbox = ctk.CTkCheckBox(main_frame, text="Alert Monitor", variable=live_alert_var, command=update_dashboard_state)
+live_alert_checkbox.grid(row=0, column=0, padx=10, pady=10)
+
+dashboard_checkbox = ctk.CTkCheckBox(main_frame, text="Dashboard", variable=dashboard_var, state="disabled")
+dashboard_checkbox.grid(row=1, column=0, padx=10, pady=10)
+
+alert_checkbox = ctk.CTkCheckBox(main_frame, text="Alert Scroll", variable=alert_var, command=lambda: alerts_main.kickstart(alert_stop_event))
+alert_checkbox.grid(row=2, column=0, padx=10, pady=10)
+
 confirm_button = ctk.CTkButton(main_frame, text="Confirm", command=confirm_action)
 confirm_button.grid(row=2, column=0, padx=10, pady=10)
 
-alert_var.trace_add("write", update_dashboard_state)
+live_alert_var.trace_add("write", update_dashboard_state)
 
 root.mainloop()
